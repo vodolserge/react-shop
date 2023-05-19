d9d6a2630f7145c35accff41f964aa5154325fe0
sergey.vodolazhsky <sergey.vodolazhsky@ab-soft.net>
2023-05-19 16:09:46 -0400

    # Links in `Header` and `Footer` are fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer () {
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="https://google.com" target="_blank" rel="noreferrer">Repo</a>
+                    <a className="grey-text text-lighten-4 right" href="https://github.com/vodolserge/react-shop" target="_blank" rel="noreferrer">Repo</a>
                 </div>
             </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ function Header () {
     return (
         <nav className="green darken-1">
             <div className="nav-wrapper">
-                <a href="https://google.com" className="brand-logo" target="_blank" rel="noreferrer">React shop</a>
+                <a href="https://github.com/vodolserge/react-shop" className="brand-logo" target="_blank" rel="noreferrer">React shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="https://google.com" target="_blank" rel="noreferrer">Repo</a></li>
-                    <li><a href="https://google.com" target="_blank" rel="noreferrer">React: v18.2.0</a></li>
+                    <li><a href="https://github.com/vodolserge/react-shop" target="_blank" rel="noreferrer">Repo</a></li>
+                    <li><a href="https://github.com/vodolserge/react-shop" target="_blank" rel="noreferrer">React: v18.2.0</a></li>
                 </ul>
             </div>
         </nav>
