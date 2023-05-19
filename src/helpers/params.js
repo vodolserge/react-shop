ea3957e40b06060410727d087422b863f8ad36fe
sergey.vodolazhsky <sergey.vodolazhsky@ab-soft.net>
2023-05-19 16:00:36 -0400

    # config fixed

--- src/helpers/params.js
+++ src/helpers/params.js
@@ -3,5 +3,6 @@
  *************************/
 
 // export const apiKey = process.env.API_KEY;
-export const apiKey = process.env.API_KEY;
-export const apiUrl = `https://fortniteapi.io/v2/shop?lang=ru`;
+export const apiKey = '1c91bce8-1d7f56bf-98f861d7-e4aff680';
+
+export const apiUrl = 'https://fortniteapi.io/v2/shop?lang=en';
